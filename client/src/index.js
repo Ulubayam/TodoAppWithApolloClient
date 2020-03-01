0027883b3c1088719eb49f2edf7c84416603d153
gizem.ulubayam <gizem.ulubayam@cimri.com>
2020-03-01 22:42:21 +0300

    added gitignore and bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import ApolloClient from "apollo-boost";
+import "bootstrap/dist/css/bootstrap.css";
 import { ApolloProvider } from "@apollo/react-hooks";
 import * as serviceWorker from "./serviceWorker";
 
