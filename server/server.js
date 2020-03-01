c9a02f5c8810fc17bfd481a4822f9031738ff201
gizem.ulubayam <gizem.ulubayam@cimri.com>
2020-03-01 16:39:47 +0300

    server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,64 @@
-import express from "express";
-import { ApolloServer, gql } from "apollo-server-express";
-import cors from "cors";
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+const cors = require("cors");
+
+let todos = [
+  {
+    id: Date.now().toString(),
+    text: "Hello from GraphQL",
+    completed: true
+  }
+];
+
+const typeDefs = gql`
+  type Todo {
+    id: String
+    text: String
+    completed: Boolean
+  }
+  type Query {
+    todos: [Todo]!
+  }
+  type Mutation {
+    createTodo(text: String!): String
+    removeTodo(id: String!): String
+    updateTodo(id: String!): String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    todos: () => todos
+  },
+  Mutation: {
+    createTodo: (parent, args, context, info) => {
+      return todos.push({
+        id: Date.now().toString(),
+        text: args.text,
+        completed: false
+      });
+    },
+    removeTodo: (parent, args, context, info) => {
+      for (let i in todos) {
+        if (todos[i].id === args.id) {
+          todos.splice(i, 1);
+        }
+      }
+    },
+    updateTodo: (parent, args, context, info) => {
+      for (let i in todos) {
+        if (todos[i] === args.id) {
+          todos[i].completed = !todos[i].completed;
+        }
+      }
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+server.applyMiddleware({ app });
+
+app.use(cors());
+
+app.listen({ port: 5555 }, () => console.log("basarılı", server.graphqlPath));
