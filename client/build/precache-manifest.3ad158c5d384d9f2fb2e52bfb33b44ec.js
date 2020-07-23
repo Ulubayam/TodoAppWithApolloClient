4a71f9019ff0a842ed1762c9e031eb0e3b63e373
gizem.ulubayam <gizem.ulubayam@cimri.com>
2020-07-23 15:49:47 +0300

    added build

---
+++ client/build/precache-manifest.3ad158c5d384d9f2fb2e52bfb33b44ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4031fbf9177540f772689d81bf9037bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef8138b8b05800c15eb0",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c323c062e583bced67bd",
+    "url": "/static/css/main.d4c8cde8.chunk.css"
+  },
+  {
+    "revision": "ef8138b8b05800c15eb0",
+    "url": "/static/js/2.37795b69.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.37795b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c323c062e583bced67bd",
+    "url": "/static/js/main.81925cd0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
